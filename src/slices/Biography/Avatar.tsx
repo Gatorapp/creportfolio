de162533e903ee565e3e49892d7424554ecedadd
Amit <amit.p.patel79@gmail.com>
2025-02-26 23:25:55 -0600

    update error

--- src/slices/Biography/Avatar.tsx
+++ src/slices/Biography/Avatar.tsx
@@ -20,7 +20,7 @@ export default function Avatar({image,className,}: AvatarProps){
     const component = useRef(null)
 
     useEffect (() => {
-        let ctx = gsap.context(() => {
+        gsap.context(() => {
             gsap.fromTo(
                 ".avatar",
                 { opacity:0., scale: 1.4 },
@@ -32,11 +32,11 @@ export default function Avatar({image,className,}: AvatarProps){
                 const componentRect = (component.current as HTMLElement). getBoundingClientRect()
                 const componentCenterX = componentRect.left + componentRect.width / 2;
 
-                let componentPercent = {
+                const componentPercent = {
                     x: ( e.clientX - componentCenterX ) / componentRect.width / 2
                 }
 
-                let distFromCenter = 1 - Math.abs(componentPercent.x)
+                const distFromCenter = 1 - Math.abs(componentPercent.x)
 
                 gsap
                 .timeline({
